3d4d1f1ea8b9a0abdd4a436b4fd2764d43c20fd5
keepjihye <yoojihea60@gmail.com>
2024-10-31 19:56:25 +0900

    cursor 추가 / 이미지 변경 필요

--- resources/js/pattern/ui-pattern-script.js
+++ resources/js/pattern/ui-pattern-script.js
@@ -75,3 +75,12 @@ tl.to(".main-visual__txtBox", {
     ease: "power2.out", // easing 설정
 }, 0);
 
+// 커서 효과
+document.addEventListener('mousemove', (e) => {
+    let mouseX = e.pageX + 10; // document의 x좌표값
+    let mouseY = e.pageY + 10; // document의 y좌표값
+
+    let cursor = document.querySelector('.cursor');
+    cursor.style.left = mouseX + 'px';
+    cursor.style.top = mouseY + 'px';
+})
